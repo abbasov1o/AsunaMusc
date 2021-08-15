d6629ac2e741a19c44e3ba5c07a4d08a6aafe776
K i r i t o <81063535+abbasov1o@users.noreply.github.com>
2021-08-15 17:58:49 +0400

    Update AsunaMusic.py

--- AsunaMusic.py
+++ AsunaMusic.py
@@ -109,7 +109,7 @@ def a(client, message):
                 ]
             )
     
-        message.reply_audio(audio_file, reply_markup, caption=rep, parse_mode='md', quote=False, title=title, thumb=thumb_name)
+        message.reply_audio(audio_file, reply_markup, caption=rep, parse_mode='md', title=title, thumb=thumb_name)
         m.delete()
     except Exception as e:
         m.edit('❌Xəta\n\n Xətanı bildirmək üçün @abbasov1o ❤️')
