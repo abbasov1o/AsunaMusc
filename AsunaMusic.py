7a071eea0f99f315d78142c38ac1cc0480e7f726
K i r i t o <81063535+abbasov1o@users.noreply.github.com>
2021-08-15 09:47:33 +0400

    Update AsunaMusic.py

--- AsunaMusic.py
+++ AsunaMusic.py
@@ -105,7 +105,7 @@ def a(client, message):
                 ]
             ]
         )
-    )
+    
         message.reply_audio(audio_file, caption=rep, parse_mode='md',quote=False, title=title, duration=dur, thumb=thumb_name)
         m.delete()
     except Exception as e:
