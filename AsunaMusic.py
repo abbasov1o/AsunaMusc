d6e2920285220411cc70dbf5610622f86456b45f
K i r i t o <81063535+abbasov1o@users.noreply.github.com>
2021-08-15 09:39:28 +0400

    Update AsunaMusic.py

--- AsunaMusic.py
+++ AsunaMusic.py
@@ -92,7 +92,6 @@ def a(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f'🎧Başlıq: [{title[:35]}]({link})\n⏳Müddət: `{duration}`\n🎬Mənbə: [Youtube]({link})'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
