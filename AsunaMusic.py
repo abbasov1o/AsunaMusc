7533c23fd7d9d5bcab0b6ff00f60b07903bb59b3
K i r i t o <81063535+abbasov1o@users.noreply.github.com>
2021-08-15 16:20:40 +0400

    Update AsunaMusic.py

--- AsunaMusic.py
+++ AsunaMusic.py
@@ -111,7 +111,7 @@ def a(client, message):
             ]
         ]
     )
-    await message.reply_audio_file(
+    await message.reply_ydl(
     reply_markup=butts,
     caption=rep,
         secmul, dur, dur_arr = 1, 0, duration.split(':')
