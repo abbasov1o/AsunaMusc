8ac39f6c8c2d3fe7343a633578e588aa5773e59e
K i r i t o <81063535+abbasov1o@users.noreply.github.com>
2021-08-15 09:44:22 +0400

    Update AsunaMusic.py

--- AsunaMusic.py
+++ AsunaMusic.py
@@ -96,7 +96,7 @@ def a(client, message):
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
             secmul *= 60
-        message.reply_audio( 
+        message.reply_audio_file( 
         quote=False,
         reply_markup=InlineKeyboardMarkup(
             [
