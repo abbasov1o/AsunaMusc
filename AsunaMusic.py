54aa533ba249ef710044f8e1d2a615a4e6ab39a3
K i r i t o <81063535+abbasov1o@users.noreply.github.com>
2021-08-16 06:25:06 +0400

    Update AsunaMusic.py

--- AsunaMusic.py
+++ AsunaMusic.py
@@ -23,7 +23,7 @@ TG = Client(
     api_hash = Config.API_HASH
 )
 
-def bmusiqi():
+def ba():
 	BUTTON=[[InlineKeyboardButton(text="🔊 Rəsmi Kanal", url="https://t.me/zenmusiqi")]]
 	BUTTON+=[[InlineKeyboardButton(text="➕ Grupa Əlavə Et ➕", url=f"https://t.me/song_azbot?startgroup=true")]]
 	return InlineKeyboardMarkup(BUTTON)
