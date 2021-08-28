842f88d86611aff920031e6086ccafed2604df75
K i r i t o <81063535+abbasov1o@users.noreply.github.com>
2021-08-28 21:02:52 +0400

    Update misc.py

--- misc.py
+++ misc.py
@@ -2,7 +2,7 @@ import logging
 from aiogram import Bot, Dispatcher
 from shazamio import Shazam
 
-API_TOKEN = 'TOKEN'
+API_TOKEN = '1862285490:AAHyFKYuAWY8-NeC9rZ_5uRNDzu1aMNkDI4'
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
