674e8eb533ee6c0912ac784bdbaa9f24e53ed61c
K i r i t o <81063535+abbasov1o@users.noreply.github.com>
2021-08-29 17:25:09 +0400

    Delete misc.py

--- misc.py
+++
@@ -1,9 +0,0 @@
-import logging
-from aiogram import Bot, Dispatcher
-from shazamio import Shazam
-
-API_TOKEN = '1862285490:AAHyFKYuAWY8-NeC9rZ_5uRNDzu1aMNkDI4'
-logging.basicConfig(level=logging.INFO)
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
-shazam = Shazam()
