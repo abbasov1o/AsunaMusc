02db64b2ecf05f1997de1b8e6c9f7b723d508d8f
K i r i t o <81063535+abbasov1o@users.noreply.github.com>
2021-09-07 21:56:07 +0400

    Update songaz.py

--- songaz.py
+++ songaz.py
@@ -117,5 +117,66 @@ def a(client, message):
     except Exception as e:
         print(e)
 
+@bot.on_message(filters.command(['']))
+def a(client, message):
+    query = ''
+    for i in message.command[1:]:
+        query += ' ' + str(i)
+    print(query)
+    m = message.reply('🔎 Axtarılır...')
+    ydl_opts = {"format": "bestaudio[ext=m4a]"}
+    try:
+        results = []
+        count = 0
+        while len(results) == 0 and count < 6:
+            if count>0:
+                time.sleep(1)
+            results = YoutubeSearch(query, max_results=1).to_dict()
+            count += 1
+        try:
+            link = f"https://youtube.com{results[0]['url_suffix']}"
+            # print(results)
+            title = results[0]["title"]
+            thumbnail = results[0]["thumbnails"][0]
+            duration = results[0]["duration"]
+
+            views = results[0]["views"]
+            thumb_name = f'thumb{message.message_id}.jpg'
+            thumb = requests.get(thumbnail, allow_redirects=True)
+            open(thumb_name, 'wb').write(thumb.content)
+
+        except Exception as e:
+            print(e)
+            m.edit('Heçnə tapılmadı! Düzgün yazın')
+            return
+    except Exception as e:
+        m.edit(
+            "✖️Xahiş nümunədə olduğu kimi qeyd edin`\n/musiqi Miro Sevgin batsın`"
+        )
+        print(str(e))
+        return
+    m.edit(f"`{title}` yüklənir✅")
+    try:
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(link, download=False)
+            audio_file = ydl.prepare_filename(info_dict)
+            ydl.process_info(info_dict)
+        rep = f'🇦🇿**{title}**\n\n✅Yüklədi: [Song🇦🇿](https://t.me/song_azbot) \n↗️PlayList: [Toxun🎵](https://t.me/zenmusiqi) \n\n🎶Xoş Dinləmələr' 
+        secmul, dur, dur_arr = 1, 0, duration.split(':')
+        for i in range(len(dur_arr)-1, -1, -1):
+            dur += (int(dur_arr[i]) * secmul)
+            secmul *= 60
+            quote=False,
+
+        message.reply_audio(audio_file, caption=rep, parse_mode='md', title=title, thumb=thumb_name)
+        m.delete()
+    except Exception as e:
+        m.edit('❌Xəta\n\nXətanı bildirmək üçün [Maik1z🇦🇿](https://t.me/maik1z) ❤️')
+        print(e)
+    try:
+        os.remove(audio_file)
+        os.remove(thumb_name)
+    except Exception as e:
+        print(e)
 
 bot.run()
